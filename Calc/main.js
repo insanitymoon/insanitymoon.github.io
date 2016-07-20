70e48764a793cbe922d45e4c3b5a9e48a880c6d5
insanitymoon <cristian.badoi98@gmail.com>
2016-07-20 08:41:13 +0300

    --all

---
+++ Calc/main.js
@@ -0,0 +1,57 @@
+var buttons = document.getElementsByClassName("col");
+var panel = document.getElementById("text-input");
+
+var operators = {
+	'+': function(a, b) { return a + b},
+	'-': function(a, b) { return a - b},
+	'/': function(a, b) { return a / b},
+	'*': function(a, b) { return a * b},
+	'.': function(a, b) { return parseFloat(a.toString() + "." + b.toString())}
+};
+
+var ops = false;
+var op = "";
+var opb = 0;
+var previousEl = 0;
+
+for (var it = 0; it < buttons.length; it++) {
+	buttons[it].addEventListener("click", add);
+}
+
+var del = function () {
+	ops = false;
+	op = "";
+	opb = 0;
+	previousEl = 0;
+	panel.value = 0;
+}
+
+function add() {
+	if(!(this.className.substring(4) === "delete")) {
+		if (!ops) {
+			previousEl = panel.value;
+
+			if (!(this.className.substring(4) === "command")) {
+				panel.value = previousEl + this.innerHTML;
+			} else {
+				ops = true;
+				op = this.innerHTML.toString();
+
+				panel.value = this.innerHTML;
+			}
+		} else {
+			if (!(this.className.substring(4) === "command")) {
+				panel.value = operators[op.toString()](parseFloat(previousEl), parseFloat(this.innerHTML));
+				opb = previousEl;
+			} else {
+				panel.value = operators[op.toString()](parseFloat(previousEl), parseFloat(opb));
+
+			}
+			
+			ops = false;
+			op = "";
+		}
+	} else {
+		del();
+	}
+}
